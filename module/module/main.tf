31606c59749c52c104a7e63627bb56897047931d
KarishmaMPatil <karipatil16@gmail.com>
2026-02-06 10:53:21 +0000

    first

---
+++ module/module/main.tf
@@ -0,0 +1,20 @@
+module "ec2" {
+  source = "/home/karipatil/TERRAFORMVPCPROJECT/terraformb32/module/main/ec2"
+  webserver_ami = "ami-019715e0d74f695be"   
+   webserver_instance_type = "t3.micro"   
+  webserver_keyname = "ubuntu"
+  webserver_disable_api_termination = false
+  webserver_sg = module.vpc.webserversg   #attache security group to ec2 module name vpc webserversg from output block
+  webserver_subnetA = module.vpc.subnet_id  #subnet id from output block 
+
+
+} 
+
+module "vpc" {
+  source =  "/home/karipatil/TERRAFORMVPCPROJECT/terraformb32/module/main/vpc"
+  vpc_cidr = "172.30.0.0/16"   #vpc variable block unique name
+  vpc_subnetA = "172.30.128.0/20"
+  public_ip = true
+  az = "ap-south-1a"
+
+}
